dcff46455e3247204db4d233a522a95643525911
getnamo <getnamo@getnamo.com>
2016-09-17 01:19:36 +0100

    bat fixes

--- BuildSocketIO.bat
+++ BuildSocketIO.bat
@@ -1,4 +1,6 @@
 cd src/socket.io-client-cpp
-cmake -DBOOST_ROOT:STRING=boost -DBOOST_VER:STRING=1.60 ./
-make install
+REM cmake -DBOOST_ROOT:STRING="./boost/include" -DBOOST_VER:STRING="1.60" ./
+
+cmake -DBOOST_INCLUDEDIR="./boost/include" -DBOOST_LIBRARYDIR="./boost/lib" -DBOOST_VER:STRING="1.60" ./
+cmake make install
 pause
--- MoveFiles.bat
+++ MoveFiles.bat
@@ -1,5 +1,5 @@
 xcopy "src/boost/boost_output/lib" "Output/Boost/Lib" /e /i /h
 
-xcopy "src/boost/boost_output/include/boost-1_60" "src/socket.io-client-cpp/boost/include" /e /i /h >NUL
+xcopy "src/boost/boost_output/include/boost-1_60" "src/socket.io-client-cpp/boost/include" /e /i /h
 xcopy "src/boost/boost_output/lib" "src/socket.io-client-cpp/boost/lib" /e /i /h
 pause
