9fed3d7aff100ab68f0153a289bf656975559a7b
getnamo <getnamo@gmail.com>
2018-02-24 18:50:08 +0000

    copy sioclient libs

--- Windows/MoveSocketFiles.bat
+++ Windows/MoveSocketFiles.bat
@@ -4,4 +4,7 @@ echo f | xcopy /f /y "../src/socket.io-client-cpp/src/sio_socket.h" "../Output/S
 
 echo f | xcopy /f /y "../src/socket.io-client-cpp/src/internal/sio_packet.h" "../Output/SocketIO/Include/internal/sio_packet.h"
 echo f | xcopy /f /y "../src/socket.io-client-cpp/src/internal/sio_client_impl.h" "../Output/SocketIO/Include/internal/sio_client_impl.h"
+
+echo f | xcopy /f /y "../src/socket.io-client-cpp/Release/sioclient.lib" "../Output/SocketIO/Lib/Win32/sioclient.lib"
+echo f | xcopy /f /y "../src/socket.io-client-cpp/x64/Release/sioclient.lib" "../Output/SocketIO/Lib/Win64/sioclient.lib"
 pause
